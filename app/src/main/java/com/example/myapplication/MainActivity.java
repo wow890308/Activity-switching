79406f47f5f2949cc91cc8f952cfb1bdda76f30a
wow890308 <wow890308@gmail.com>
2021-11-09 12:47:03 +0800

    switch

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.BackgroundServiceStartNotAllowedException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Aswitch(View view){
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
